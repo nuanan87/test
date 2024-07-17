6bfa5b8b511b4432e0a5c805b41aca83f1a423ad
xinaimei87@qq.com <xinaimei87@qq.com>
2024-07-17 16:05:44 +0800

    'developA修改txt为冲突内容'

--- Cat/BuOu/main/java/NuanAn.java
+++ Cat/BuOu/main/java/NuanAn.java
@@ -10,4 +10,27 @@ public class NuanAn {
     private String sex;
     private Integer age;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
