9164b095521af3819e9c5cccf28addbafcab80fb
xinaimei87@qq.com <xinaimei87@qq.com>
2024-07-17 15:37:15 +0800

    'NuanAn提交'

---
+++ Cat/BuOu/main/java/NuanAn.java
@@ -0,0 +1,13 @@
+/**
+ * @author shawn
+ * @version 1.0
+ * @ClassName NuanAn
+ * Description:
+ * @date 2024/7/17 15:34
+ */
+public class NuanAn {
+    private String name;
+    private String sex;
+    private Integer age;
+
+}
